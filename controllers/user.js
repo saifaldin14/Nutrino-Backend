04faf9773a666e4920dc7fa2b17790bfc72f47f9
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-06 18:31:30 -0500

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -70,3 +70,36 @@ exports.userSignIn = async (req, res) => {
 
   res.json({ success: true, user: userInfo, token });
 };
+
+exports.uploadProfile = async (req, res) => {
+  const { user } = req;
+
+  if (!user)
+    return res
+      .status(401)
+      .json({ success: false, message: "unauthorized access!" });
+
+  try {
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      public_id: `${user._id}_profile`,
+      width: 500,
+      height: 500,
+      crop: "fill",
+    });
+
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      { avatar: result.url },
+      { new: true }
+    );
+
+    res
+      .status(201)
+      .json({ success: true, message: "Your profile has updated!" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "server error, try after some time" });
+    console.log("Error while uploading profile image", error.message);
+  }
+};
