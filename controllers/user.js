e4bad0a1042cd30e5b59fd7d1c7db4aad2bf9ebd
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-06 11:40:42 -0500

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -23,3 +23,23 @@ exports.createUser = async (req, res) => {
   await user.save();
   res.json({ success: true, user });
 };
+
+exports.userSignIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    return res.json({
+      success: false,
+      message: "user not found, with the given email!",
+    });
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch)
+    return res.json({
+      success: false,
+      message: "email / password does not match!",
+    });
+};
